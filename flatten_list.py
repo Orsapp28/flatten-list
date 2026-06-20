89a693e95a7f64c33c224eb5574c65adce8c9f1b
Orane <oranesappleton2009@live.com>
2026-06-20 16:25:02 -0400

    Completed the flatten list challenge

--- flatten_list.py
+++ flatten_list.py
@@ -1,2 +1,26 @@
-def flatten_list(lst):
-    pass
+def flatten_list(lst, flat_result=None):
+
+    if flat_result is None:
+        flat_result = []
+
+    for item in lst:
+
+        if type(item) is list:
+            flatten_list(item, flat_result)
+
+        else:
+            flat_result.append(item)
+
+    return flat_result
+
+
+print(flatten_list([1, 2, [3], [4, 5]]))
+
+
+print(flatten_list([1, [2], [3, [4, 5, [6]]], 7]))
+
+
+print(flatten_list([1, 2, 3]))
+
+
+print(flatten_list([]))
